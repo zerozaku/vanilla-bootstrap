a152de0f486da3fe051ba443dab5f89dff003c3e
anshin <anshin@ymail.com>
2014-08-08 20:34:22 -0500

    update Vanilla copyright notice based on their README

--- views/modules/newdiscussion.php
+++ views/modules/newdiscussion.php
@@ -1,14 +1,13 @@
 <?php if (!defined('APPLICATION')) exit();
 
 /*
-Copyright (C) 2014 Vanilla Forums, Inc
+Copyright (C) 2009-2014 Vanilla Forums, Inc
 
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-The source code can be found [here](https://github.com/vanilla/vanilla), and the full text of the license [here](http://www.opensource.org/licenses/gpl-2.0.php)
+The source code can be found [here](https://github.com/vanilla/vanilla), and the full text of the license [here](http://opensource.org/licenses/gpl-3.0.html)
 */
 
 echo '<div class="BoxButtons BoxNewDiscussion">';
